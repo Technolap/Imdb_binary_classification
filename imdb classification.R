e58b01878fc6fd2e53b044889b2851b6a23351df
Mohammed Safwan Salman <156708093+Technolap@users.noreply.github.com>
2026-02-04 22:14:58 -0330

    splitted the trained data and the test data and performed word index mapping and reverse word index mapping

--- imdb classification.R
+++ imdb classification.R
@@ -8,4 +8,19 @@ library(keras3)
 # loading the Imdb dataset Load IMDB dataset with only the first 10,000 words
 imdb <- dataset_imdb(num_words = 10000)
 
+# splitting into train / test data and labels
+c(c(train_data, train_labels), c(test_data, test_labels)) %<-% imdb
 
+# checking the structure of the trained data and labels
+str(train_data)
+str(train_labels)
+
+# finding the maximum word index 
+max(sapply(train_data, max))
+
+# getting the word index mapping from word to integer 
+word_index <- dataset_imdb_word_index()
+
+# performing a reverse word index mapping from integer to word 
+reverse_word_index <- names(word_index)
+names(reverse_word_index) <- as.character(word_index)
