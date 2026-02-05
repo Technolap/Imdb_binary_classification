ead98327e6ccbf391dde6acab0015378fa920798
Mohammed Safwan Salman <156708093+Technolap@users.noreply.github.com>
2026-02-04 22:21:15 -0330

    created function to vectorize sequences and performed vectorized training and test data

--- imdb classification.R
+++ imdb classification.R
@@ -24,3 +24,33 @@ word_index <- dataset_imdb_word_index()
 # performing a reverse word index mapping from integer to word 
 reverse_word_index <- names(word_index)
 names(reverse_word_index) <- as.character(word_index)
+
+
+# decoding the first review back into words 
+decoded_words <- train_data[[1]] %>%
+  sapply(function(i) {
+    if (i > 3) reverse_word_index[[as.character(i - 3)]]
+    else "?"  
+  })
+
+# combining the words into a readable review 
+decoded_review <- paste0(decoded_words, collapse = " ")
+cat(decoded_review, "\n")
+
+# function for vectorizing sequences into binary group of words
+vectorize_sequences <- function(sequences, dimension = 10000) {
+  results <- array(0, dim = c(length(sequences), dimension))
+  for (i in seq_along(sequences)) {
+    sequence <- sequences[[i]]
+    for (j in sequence)
+      results[i, j] <- 1 
+  }
+  results
+}
+
+# vectorizing the train and the test data 
+x_train <- vectorize_sequences(train_data)
+x_test <- vectorize_sequences(test_data)
+
+# displaying the structure of the vectoried trained data 
+str(x_train)
